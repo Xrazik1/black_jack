a3d7f278de31ffc825daa80971a8a54bcf685d95
Nikolay <Xrazik@yandex.ru>
2019-11-01 18:59:33 +0300

    Common methods moved from dealer to member

--- member.rb
+++ member.rb
@@ -1,12 +1,41 @@
 # frozen_string_literal: true
 
+require_relative 'card'
+
 class Member
-  attr_accessor :bank
-  attr_accessor :cards
+  attr_accessor :bank, :cards
 
   def initialize
     @cards = []
     @bank = 100
+    @score = 0
+  end
+
+  def add_card(card)
+    @cards << card
+    @score = read_cards_sum
+  end
+
+  def remove_card(card)
+    @cards.delete(card)
+    @score = read_cards_sum
+  end
+
+  def read_cards_sum
+    sum = 0
+    (@cards.reject { |card| card.value == 'A' }).each { |card| sum += card.card_worth(card.value) }
+    sum + read_aces_sum(sum)
+  end
+
+  protected
+
+  def read_aces_sum(init_sum)
+    sum = 0
+    if init_sum + 11 > 21
+      (@cards.select { |card| card.value == 'A' }).each { |ace| sum += ace.card_worth('A')[0] }
+    else
+      (@cards.select { |card| card.value == 'A' }).each { |ace| sum += ace.card_worth('A')[1] }
+    end
+    sum
   end
 end
-
