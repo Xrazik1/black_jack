62ae732ba0bec3542b8f16a4dc5ba304fc26afd5
Nikolay <Xrazik@yandex.ru>
2019-11-01 19:00:15 +0300

    Common methods moved from dealer to member

--- dealer.rb
+++ dealer.rb
@@ -8,27 +8,10 @@ class Dealer < Member
     super()
   end
 
-  def read_cards_sum
-    sum = 0
-    (@cards.reject { |card| card.value == 'A' }).each { |card| sum += card.card_worth(card.value) }
-    sum + read_aces_sum(sum)
-  end
-
   def make_move
 
   end
 
-  private
-
-  def read_aces_sum(init_sum)
-    sum = 0
-    if init_sum + 11 > 21
-      (@cards.select { |card| card.value == 'A' }).each { |ace| sum += ace.card_worth('A')[0] }
-    else
-      (@cards.select { |card| card.value == 'A' }).each { |ace| sum += ace.card_worth('A')[1] }
-    end
-    sum
-  end
 end
 
 dealer = Dealer.new
