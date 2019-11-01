3bfd1bb53ae1c368f80743a460f66256f46df12c
Nikolay <Xrazik@yandex.ru>
2019-11-01 20:35:15 +0300

    Now you can make bet on the table

--- table.rb
+++ table.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative 'card'
-
+require_relative 'dealer'
 
 class Table
   attr_reader :deck, :players
@@ -9,12 +9,23 @@ class Table
   def initialize(player, dealer)
     @bank = 0
     @deck = init_deck
-    @players = [player, dealer]
+    @player = player
+    @dealer = dealer
   end
 
   def take_card
     card = @deck[0]
     @deck.delete(card)
+    card
+  end
+
+  def make_bet(bet = 10)
+    raise 'У игрока недостаточно средств для совершения ставки' if (@player.bank - bet).negative?
+    raise 'У дилера недостаточно средств для совершения ставки' if (@dealer.bank - bet).negative?
+
+    @player.bank -= bet
+    @dealer.bank -= bet
+    @bank += bet * 2
   end
 
   private
