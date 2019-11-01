1cd48ab8951a296f9ca319af442216402998a5bc
Nikolay <Xrazik@yandex.ru>
2019-11-01 17:01:11 +0300

    Now can define the sum of cards and aces

--- dealer.rb
+++ dealer.rb
@@ -1,10 +1,35 @@
 # frozen_string_literal: true
 
 require_relative 'member'
+require_relative 'card'
 
-class Dealer
+class Dealer < Member
   def initialize
     super()
+  end
+
+  def read_cards_sum
+    sum = 0
+    (@cards.reject { |card| card.value == 'A' }).each { |card| sum += card.card_worth(card.value) }
+    sum + read_aces_sum(sum)
+  end
+
+  def make_move
 
   end
+
+  private
+
+  def read_aces_sum(init_sum)
+    sum = 0
+    if init_sum + 11 > 21
+      (@cards.select { |card| card.value == 'A' }).each { |ace| sum += ace.card_worth('A')[0] }
+    else
+      (@cards.select { |card| card.value == 'A' }).each { |ace| sum += ace.card_worth('A')[1] }
+    end
+    sum
+  end
 end
+
+dealer = Dealer.new
+puts dealer.read_cards_sum
