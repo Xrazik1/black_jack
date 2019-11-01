9bf83a3fcab2c92097625e0f4ff9632fbbe563c6
Nikolay <Xrazik@yandex.ru>
2019-11-01 19:40:08 +0300

    Added deck initializing in table

--- card.rb
+++ card.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 class Card
-  attr_reader :value, :suite
+  attr_reader :value, :suit
 
   POSSIBLE_SUITS = %w[♥️ ♠️ ♣️ ♦️].freeze
   POSSIBLE_VALUES = %w[2 3 4 5 6 7 8 9 10 J Q K A].freeze
 
-  def initialize(value, suite)
+  def initialize(value, suit)
     @value = value
-    @suite = suite
+    @suit = suit
 
-    unless POSSIBLE_SUITS.include?(suite) || POSSIBLE_VALUES.include?(value)
+    unless POSSIBLE_SUITS.include?(suit) || POSSIBLE_VALUES.include?(value)
       raise 'Карту с такой мастью и достоинством создать невозможно'
     end
   end
--- table.rb
+++ table.rb
@@ -1,8 +1,31 @@
 # frozen_string_literal: true
 
+require_relative 'card'
+
+
 class Table
+  attr_reader :deck
+
   def initialize
     @bank = 0
-    
+    @deck = init_deck
+  end
+
+  private
+
+  def init_deck
+    deck = []
+    Card::POSSIBLE_VALUES.each do |value|
+      Card::POSSIBLE_SUITS.each do |suit|
+        deck << Card.new(value, suit)
+      end
+    end
+    deck.shuffle
   end
 end
+
+table = Table.new
+table.deck.each do |card|
+  puts card.value + card.suit
+end
+
