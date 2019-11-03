ec83c89c93b676265c19dcac36749656bf085488
Nikolay <Xrazik@yandex.ru>
2019-11-03 18:12:51 +0300

    Added main and init_game functions

--- main.rb
+++ main.rb
@@ -5,4 +5,28 @@ require_relative 'dealer'
 require_relative 'player'
 require_relative 'ui'
 
+game = {}
 
+def init_game(game)
+  return game unless game.empty?
+
+  ui = Ui.new
+  dealer = Dealer.new
+  player = Player.new(ui.take_name)
+  table = Table.new(player, dealer, 10)
+
+  { dealer: table.dealer, player: table.player, table: table, ui: ui }
+end
+
+def main(game)
+  game = init_game(game)
+
+  puts "Здравствуйте, #{game[:player].name}, добро пожаловать в игру блэкджек"
+  puts "Ваш баланс: #{game[:player].bank}$"
+  game[:ui].ask_for_continue(game[:table].round)
+  game[:table].make_bet
+  puts "В банк сделана ставка в размере #{game[:table].bet}$"
+  puts 'Выполняется раздача карт...'
+end
+
+main(game)
