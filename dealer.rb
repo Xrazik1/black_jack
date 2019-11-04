41b93efb14f888ad0999fb16fc558c85ce706f0f
Nikolay <Xrazik@yandex.ru>
2019-11-04 05:08:53 +0300

    Added flag returning if dealer skips the step

--- dealer.rb
+++ dealer.rb
@@ -12,6 +12,7 @@ class Dealer < Member
   end
 
   def make_move(table)
-    table.deal_cards(1, self) if @score < 17
+    table.deal_cards(1, self) if (@score < 17) && (cards.size < 3)
+    'skip' if @score > 17
   end
 end
